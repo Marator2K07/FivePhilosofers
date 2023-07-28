36853ab8ee400aa5b6300f853c0d3aaae92e823f
Marator2K207 <dmitriyshaaa@gmail.com>
2023-07-28 15:03:16 +0300

    Создание графического представления текста как атрибута-ребенка для класса GraphicsPhilosoferItem. Создание спец. метода для отложенной его инициализации. Мелкие правки.

--- graphicsphilosoferitem.cpp
+++ graphicsphilosoferitem.cpp
@@ -7,7 +7,22 @@ GraphicsPhilosoferItem::GraphicsPhilosoferItem(QString name,
     , name{name}
 {
     // инициализируем цвет границы и цвет заливки для этого элемента
-    setPen(QPen(Qt::black, 5));
+    setPen(QPen(Qt::black, width));
     setBrush(QBrush(QColor(color)));
+}
+
+void GraphicsPhilosoferItem::statusInitialization()
+{
+    // сначала просто инициализация задание ширины для текста
+    status = new GraphicsCenteredTextItem(this);
+    status->setTextWidth(this->boundingRect().width());
+    // указание верной позиции
+    QRectF parentRect = this->boundingRect();
+    status->setPos(parentRect.topLeft().rx(),
+                   parentRect.topLeft().ry());
+    // задание шрифта и его применение
+    QFont sansFont("Helvetica [Cronyx]", 12);
+    status->setFont(sansFont);
+    status->setPlainText("...");
 }
 
--- graphicsphilosoferitem.h
+++ graphicsphilosoferitem.h
@@ -2,7 +2,10 @@
 #ifndef GRAPHICSPHILOSOFERITEM_H
 #define GRAPHICSPHILOSOFERITEM_H
 
+#include "graphicscenteredtextitem.h"
+
 #include <QGraphicsEllipseItem>
+#include <QGraphicsScene>
 #include <QBrush>
 #include <QPen>
 
@@ -11,9 +14,19 @@ class GraphicsPhilosoferItem : public QGraphicsEllipseItem
 private:
     QString name;
     QString color;
+    int width = 6; // ширина линии обводки(QPen)
+    GraphicsCenteredTextItem *status; // текст статуса выполняемой задачи у философа
+
 public:
     GraphicsPhilosoferItem(QString name,
                            QString color);
+    ///
+    /// \brief statusInitialization
+    /// т.к. не все данные для инициализации известны заранее
+    /// выносим метод отдельно и вызываем уже после определенных
+    /// условий
+    ///
+    void statusInitialization();
 };
 
 #endif // GRAPHICSPHILOSOFERITEM_H
