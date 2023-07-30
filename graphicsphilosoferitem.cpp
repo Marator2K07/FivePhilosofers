726ce2c28257aadb9614a96851b8e70af8751c36
Marator2K207 <dmitriyshaaa@gmail.com>
2023-07-30 15:28:02 +0300

    Изменения, касающиеся внешнего вида элемента класса GraphicsPhilosoferItem.

--- graphicsphilosoferitem.cpp
+++ graphicsphilosoferitem.cpp
@@ -15,11 +15,11 @@ void GraphicsPhilosoferItem::statusInitialization()
 {
     // сначала просто инициализация задание ширины для текста
     status = new GraphicsCenteredTextItem(this);
-    status->setTextWidth(this->boundingRect().width());
+    status->setTextWidth(this->boundingRect().width() - width);
     // указание верной позиции
     QRectF parentRect = this->boundingRect();
-    status->setPos(parentRect.topLeft().rx(),
-                   parentRect.topLeft().ry());
+    status->setPos(parentRect.topLeft().rx() + width/2,
+                   parentRect.topLeft().ry() + width/2);
     // задание шрифта и его применение
     QFont sansFont("Helvetica [Cronyx]", 14);
     status->setFont(sansFont);
