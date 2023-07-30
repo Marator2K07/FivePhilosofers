a7a6a19b7ef5054d274b9a565ad64683a648d181
Marator2K207 <dmitriyshaaa@gmail.com>
2023-07-30 21:10:36 +0300

    Реализация основного метода класса Philosopher теперь разбита на приватные методы под каждую из задач. Метод taskExecution имеет максимальный уровень абстрации.

--- philosopher.cpp
+++ philosopher.cpp
@@ -1,6 +1,125 @@
 
 #include "philosopher.h"
 
+void Philosopher::preparation(int defaultPause)
+{
+    emit changeStatus("Сел за стол",
+                      QString("Философ <font color=\"%1\" size=\"4\">%2</font> садится за стол с едой;")
+                          .arg(color, name),
+                      "grey");
+    QThread::msleep(defaultPause);
+}
+
+void Philosopher::reflection(int *time,
+                             int defaultPause)
+{
+    *time = QRandomGenerator::global()->bounded(1000,5000);
+    emit changeStatus("Начинает размышлять",
+                      QString("Философ <font color=\"%1\" size=\"4\">%2</font> "
+                              "начинает размышлять %3 миллисекунд;")
+                          .arg(color, name).arg(*time),
+                      "black");
+    QThread::msleep(*time);
+    emit changeStatus("Закончил размышлять",
+                      QString("Философ <font color=\"%1\" size=\"4\">%2</font> закончил размышлять;")
+                          .arg(color, name),
+                      "grey");
+    QThread::msleep(defaultPause);
+}
+
+void Philosopher::eating(int *time,
+                         int defaultPause)
+{
+    *time = QRandomGenerator::global()->bounded(1,5000);
+    emit changeStatus("Начинает принимать пищу",
+                      QString("Философ <font color=\"%1\" size=\"4\">%2</font> "
+                              "принимается за трепазу длительностью %3 миллисекунд;")
+                          .arg(color, name).arg(*time),
+                      "darkviolet");
+    QThread::msleep(*time);
+    emit changeStatus("Закончил принимать пищу",
+                      QString("Философ <font color=\"%1\" size=\"4\">%2</font> закончил принимать пищу;")
+                          .arg(color, name),
+                      "grey");
+    QThread::msleep(defaultPause);
+    emit changeStatus("Кладет вилки на место.",
+                      QString("Философ <font color=\"%1\" size=\"4\">%2</font> кладет вилки на место;")
+                          .arg(color, name),
+                      "grey");
+    QThread::msleep(defaultPause);
+    emit putRightFork();
+    rightMutex->unlock();
+    emit putLeftFork();
+    leftMutex->unlock();
+}
+
+void Philosopher::successfulUseLeftFork(int defaultPause)
+{
+    emit takeLeftFork(color);
+    emit changeStatus("Успешно взял левую вилку",
+                      QString("Философ <font color=\"%1\" size=\"4\">%2</font> успешно взял левую вилку;")
+                          .arg(color, name),
+                      "mediumslateblue");
+    QThread::msleep(defaultPause);
+}
+
+void Philosopher::successfulUseRightFork(int defaultPause)
+{
+    emit takeRightFork(color);
+    emit changeStatus("Успешно взял правую вилку",
+                      QString("Философ <font color=\"%1\" size=\"4\">%2</font> успешно взял правую вилку;")
+                          .arg(color, name),
+                      "mediumslateblue");
+    QThread::msleep(defaultPause);
+}
+
+void Philosopher::failedUseLeftFork(int defaultPause)
+{
+    emit changeStatus("Не смог взять левую вилку",
+                      QString("Философ <font color=\"%1\" size=\"4\">%2</font> не смог взять левую вилку;")
+                          .arg(color, name),
+                      "grey");
+    QThread::msleep(defaultPause);
+}
+
+void Philosopher::failedUseRightFork(int defaultPause)
+{
+    emit changeStatus("Не смог взять правую вилку",
+                      QString("Философ <font color=\"%1\" size=\"4\">%2</font> не смог взять правую вилку;")
+                          .arg(color, name),
+                      "grey");
+    QThread::msleep(defaultPause);
+    emit changeStatus("Кладет левую вилку обратно",
+                      QString("Философ <font color=\"%1\" size=\"4\">%2</font> сдается и кладет левую вилку обратно на стол;")
+                          .arg(color, name),
+                      "grey");
+    QThread::msleep(defaultPause);
+    emit putLeftFork();
+    leftMutex->unlock();
+}
+
+void Philosopher::tryTakeLeftFork(bool *blocked,
+                                  int defaultPause)
+{
+    emit changeStatus("Пытается взять левую вилку",
+                      QString("Философ <font color=\"%1\" size=\"4\">%2</font> пытается взять левую вилку;")
+                          .arg(color, name),
+                      "grey");
+    QThread::msleep(defaultPause);
+    *blocked = leftMutex->try_lock();
+}
+
+void Philosopher::tryTakeRightFork(bool *blocked,
+                                   int defaultPause)
+{
+    emit changeStatus("Пытается взять правую вилку",
+                      QString("Философ <font color=\"%1\" size=\"4\">%2</font> пытается взять правую вилку;")
+                          .arg(color, name),
+                      "grey");
+    QThread::msleep(defaultPause);
+    *blocked = rightMutex->try_lock();
+}
+
 Philosopher::Philosopher(Fork *forkLeft,
                          Fork *forkRight,
                          QMutex *leftMutex,
@@ -45,108 +164,32 @@ void Philosopher::taskExecution()
     int time;
     int defaultPause = 1111;
     // подготовительная часть
-    emit changeStatus("Сел за стол",
-                      QString("Философ <font color=\"%1\" size=\"4\">%2</font> садится за стол с едой;")
-                          .arg(color, name),
-                      "grey");    
-    QThread::msleep(defaultPause);
+    preparation(defaultPause);
     // прелюдия закончилась, теперь начинается основное действие
     while(true) {
-        time = QRandomGenerator::global()->bounded(1000,5000);
-        emit changeStatus("Начинает размышлять",
-                          QString("Философ <font color=\"%1\" size=\"4\">%2</font> "
-                                  "начинает размышлять %3 миллисекунд;")
-                              .arg(color, name).arg(time),
-                          "black");
-        QThread::msleep(time);
-        emit changeStatus("Закончил размышлять",
-                          QString("Философ <font color=\"%1\" size=\"4\">%2</font> закончил размышлять;")
-                              .arg(color, name),
-                          "grey");
-        QThread::msleep(defaultPause);
-        emit changeStatus("Пытается взять левую вилку",
-                          QString("Философ <font color=\"%1\" size=\"4\">%2</font> пытается взять левую вилку;")
-                              .arg(color, name),
-                          "grey");
-        QThread::msleep(time);
-        blocked = leftMutex->try_lock();
+        reflection(&time, defaultPause);
+        tryTakeLeftFork(&blocked, defaultPause);
+        // если левая вилка была доступна - мы ее взяли и заблокировали мьютексом
         if (blocked) {
-            emit takeLeftFork(color);
-            emit changeStatus("Успешно взял левую вилку",
-                              QString("Философ <font color=\"%1\" size=\"4\">%2</font> успешно взял левую вилку;")
-                                  .arg(color, name),
-                              "mediumslateblue");
-            QThread::msleep(defaultPause);
-            time = QRandomGenerator::global()->bounded(1000,5000);
-            emit changeStatus("Начинает размышлять",
-                              QString("Философ <font color=\"%1\" size=\"4\">%2</font> "
-                                      "начинает размышлять %3 миллисекунд;")
-                                  .arg(color, name).arg(time),
-                              "black");
-            QThread::msleep(time);
-            emit changeStatus("Закончил размышлять",
-                              QString("Философ <font color=\"%1\" size=\"4\">%2</font> закончил размышлять;")
-                                  .arg(color, name),
-                              "grey");
-            QThread::msleep(defaultPause);
-            emit changeStatus("Пытается взять правую вилку",
-                              QString("Философ <font color=\"%1\" size=\"4\">%2</font> пытается взять правую вилку;")
-                                  .arg(color, name),
-                              "grey");
-            QThread::msleep(time);
-            blocked = rightMutex->try_lock();
+            successfulUseLeftFork(defaultPause);
+            reflection(&time, defaultPause);
+            tryTakeRightFork(&blocked, defaultPause);
+            // теперь если правая вилка доступна, то мы ее берем и блокируем доступ другим потокам
             if (blocked) {
-                emit takeRightFork(color);
-                emit changeStatus("Успешно взял правую вилку",
-                                  QString("Философ <font color=\"%1\" size=\"4\">%2</font> успешно взял правую вилку;")
-                                      .arg(color, name),
-                                  "mediumslateblue");
-                QThread::msleep(defaultPause);
-                time = QRandomGenerator::global()->bounded(1,5000);
-                emit changeStatus("Начинает принимать пищу",
-                                  QString("Философ <font color=\"%1\" size=\"4\">%2</font> "
-                                          "принимается за трепазу длительностью %3 миллисекунд;")
-                                      .arg(color, name).arg(time),
-                                  "darkviolet");
-                QThread::msleep(time);
-                emit changeStatus("Закончил принимать пищу",
-                                  QString("Философ <font color=\"%1\" size=\"4\">%2</font> закончил принимать пищу;")
-                                      .arg(color, name),
-                                  "grey");
-                QThread::msleep(defaultPause);
-                emit changeStatus("Кладет вилки на место.",
-                                  QString("Философ <font color=\"%1\" size=\"4\">%2</font> кладет вилки на место;")
-                                      .arg(color, name),
-                                  "grey");
-                QThread::msleep(defaultPause);
-                emit putRightFork();
-                rightMutex->unlock();
+                successfulUseRightFork(defaultPause);
+                eating(&time, defaultPause); // употребление пищи и уборка столовых приборов (разблокировка мьютексов)
             }
+            // в случае неудачной попытки взять правую вилку, философ сдается и кладет левую вилку
+            // чтобы не мешать другими и как лишняя страховка против дедлоков
             else {
-                emit changeStatus("Не смог взять правую вилку",
-                                  QString("Философ <font color=\"%1\" size=\"4\">%2</font> не смог взять правую вилку;")
-                                      .arg(color, name),
-                                  "grey");
-                QThread::msleep(defaultPause);
-                emit changeStatus("Кладет левую вилку обратно",
-                                  QString("Философ <font color=\"%1\" size=\"4\">%2</font> сдается и кладет левую вилку обратно на стол;")
-                                      .arg(color, name),
-                                  "grey");
-                QThread::msleep(defaultPause);
+                failedUseRightFork(defaultPause);
             }
-            emit putLeftFork();
-            leftMutex->unlock();
         }
+        // в случае неудачной попытки взять левую вилку, то философ просто продолжает
+        // дальше размышлять
         else {
-            emit changeStatus("Не смог взять левую вилку",
-                              QString("Философ <font color=\"%1\" size=\"4\">%2</font> не смог взять левую вилку;")
-                                  .arg(color, name),
-                              "grey");
-            QThread::msleep(defaultPause);
+            failedUseLeftFork(defaultPause);
         }
-
-        //rightForkLocker.lock();
-        //emit takeRightFork(color);
     }
 }
 
--- philosopher.h
+++ philosopher.h
@@ -23,6 +23,59 @@ private:
     QMutex *rightMutex;
     PhilosoferGraphics *graphics; // графическое схематическое представление философа
 
+private:
+    ///
+    /// \brief preparation вынесена часть, носящая просто подготовительный,
+    /// ознакомительный характер
+    /// \param defaultPause значение паузы между сообщениями в миллисекундах
+    void preparation(int defaultPause);
+    ///
+    /// \brief reflection метод с частью, где философ начинает и
+    /// спустя какое то случаное время заканчивает свои размышления
+    /// после какой-либо операции, успешной или нет
+    /// \param time для определения и запоминания случайного значения
+    /// длительности размышлений
+    /// \param defaultPause значение паузы между сообщениями в миллисекундах
+    void reflection(int *time,
+                    int defaultPause);
+    ///
+    /// \brief eating часть, где философ успешно берет две вилки
+    /// и начинает свою трапезу, заканчивает и убирает вилки на место
+    /// \param time для определения и запоминания случайного значения
+    /// длительности трапезы
+    /// \param defaultPause значение паузы между сообщениями в  миллисекундах
+    void eating(int *time,
+                int defaultPause);
+    ///
+    /// \brief successfulUseLeftFork философ забирает левую вилку себе (помечает цветом)
+    /// \param defaultPause значение паузы между сообщениями в  миллисекундах
+    void successfulUseLeftFork(int defaultPause);
+    ///
+    /// \brief successfulUseRightFork философ забирает правую вилку себе (помечает цветом)
+    /// \param defaultPause значение паузы между сообщениями в  миллисекундах
+    void successfulUseRightFork(int defaultPause);
+    ///
+    /// \brief failedUseLeftFork оповещение пользователя о неудачности операции
+    /// \param defaultPause значение паузы между сообщениями в  миллисекундах
+    void failedUseLeftFork(int defaultPause);
+    ///
+    /// \brief failedUseRightFork оповещение пользователя о неудачности операции, но
+    /// в условиях алгоритма, на этот раз, философ еще и сдается (кладет левую вилку обратно на стол)
+    /// \param defaultPause значение паузы между сообщениями в  миллисекундах
+    void failedUseRightFork(int defaultPause);
+    ///
+    /// \brief tryTakeLeftFork попытка взять (заблокировать) левую вилку
+    /// \param blocked переменная для запоминания операции попытки блокировки
+    /// \param defaultPause значение паузы между сообщениями в  миллисекундах
+    void tryTakeLeftFork(bool *blocked,
+                         int defaultPause);
+    ///
+    /// \brief tryTakeRightFork попытка взять (заблокировать) правую вилку
+    /// \param blocked переменная для запоминания операции попытки блокировки
+    /// \param defaultPause значение паузы между сообщениями в  миллисекундах
+    void tryTakeRightFork(bool *blocked,
+                          int defaultPause);
+
 public:
     explicit Philosopher(Fork *forkLeft,
                          Fork *forkRight,
